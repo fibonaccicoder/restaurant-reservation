be66b0e03b5e9ce5067bbf6e139f0d9436787077
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-04-17 19:27:14 -0700

    working on shit

---
+++ apiRoutes.js
@@ -0,0 +1,37 @@
+app.get("/api/tables", function (req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
+
+//pushes first five tables to tables array, then following tables to waitlist array
+
+if (tables.length <= 4) {
+
+    app.post("api/tables", function (req, res) {
+        var newTable = req.body;
+
+        console.log(newTable);
+        tables.push(newTable);
+        res.json(newTable);
+    });
+} else {
+    app.post("api/waitlist", function (req, res) {
+        var newWaitlist = req.body;
+
+        console.log(waitlist);
+        waitlist.push(newWaitlist);
+        res.json(newWaitlist);
+    });
+}
+
+//
+
+
+
+
+
+module.exports = apiRoutes;
--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// home page, make reservation page, view tables page
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+
+
+var ApiRoutes = require("apiRoutes.js");
+var HtmlRoutes = require("htmlRoutes.js");
+var Waitlist = require("app/data/waitlist.js");
+var Tables = require("app/data/tables.js")
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
